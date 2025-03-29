c77d65df1eb499c4024d848cc5054dd12aae98b2
Sebastian Wallat <swallat@users.noreply.github.com>
2025-03-29 14:52:08 +0100

    Fix escaping in DOMAINS variable assignment

--- install.sh
+++ install.sh
@@ -141,7 +141,7 @@ if [ ! -f /root/.acme.sh/acme.sh ]; then
 fi
 
 # Format domains correctly for acme.sh
-DOMAINS="\$(echo \$DOMAINS | tr ',' ' ' | sed 's/[^ ]* */-d & /g')"
+DOMAINS=\"\$(echo \$DOMAINS | tr ',' ' ' | sed 's/[^ ]* */-d & /g')\"
 
 # Request and import certificates
 /root/.acme.sh/acme.sh --force --issue --dns \$SERVICE --server \$CA \$DOMAINS \\
